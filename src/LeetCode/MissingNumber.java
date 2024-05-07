a0a85a2307949ed7dc6c7006e0ecf29b7e3e4317
maksud9909 <rmaksud822@gmail.com>
2024-05-07 10:19:10 +0500

    Solved some leet code problems

--- src/LeetCode/MissingNumber.java
+++ src/LeetCode/MissingNumber.java
@@ -1,4 +1,21 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        int max = Arrays.stream(nums).max().getAsInt();
+        int [] sorted = Arrays.stream(nums).sorted().toArray();
+        for (int i = 0; i < max; i++) {
+            if (i == sorted[i]){
+                continue;
+            }
+            else {
+                res = i;
+            }
+        }
+        return res;
+
+    }
 }
